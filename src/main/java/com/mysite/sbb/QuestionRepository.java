e65e99fdd978ab343936ca6ad7cc3d845a56946b
0ssang <dimple0513@naver.com>
2023-06-22 23:31:43 +0900

    CRUD 1

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findBySubject(String subject);
+    List<Question> findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -16,18 +16,33 @@ class SbbApplicationTests {
     @Autowired //스프링의 DI 기능으로 questionRepository 객체를 스프링이 자동으로 생성해 준다.
     private QuestionRepository questionRepository;
     @Test
+    /*
+    FindAll
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
+       List<Question> all = this.questionRepository.findAll();
+       assertEquals(6, all.size());
 
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
+       Question q = all.get(0);
+       assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }*/
+    /*FindById
+    void testJpa(){
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()){
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }*/
+    //FindBySubject
+    /*void testJpa(){
+        List<Question> ql = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        Question q = ql.get(0);//리스트 중에 첫 번째 값으로 하기.
+        assertEquals(1, q.getId());
+    }*/
+    //FindBySubjectAndContent
+    void testJpa(){
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
     }
-
 }
