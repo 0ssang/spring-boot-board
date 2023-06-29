693e7db6cd3aa4eda71cbb5c4ed7acbe99257a47
0ssang <dimple0513@naver.com>
2023-06-29 23:36:45 +0900

    service 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -8,11 +8,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+    //private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
     @GetMapping("/question/list")
     public String list(Model model){
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
+        //타임리프는 model에 저장된 값을 읽을 수 있다.
         return "question_list";
     }
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+    /*
+    redirect:<url> URL로 리다이렉트 (리다이렉트는 완전히 새로운 URL로 요청이 된다.)
+    forward:<url> URL로 포워드 (포워드는 기존 요청 값들이 유지된 상태로 URL이 전환된다.)
+     */
 }
