7d07a0bd9cf027634804be61cc2d4659364b7afd
0ssang <dimple0513@naver.com>
2023-07-03 21:28:51 +0900

    Answer Service, template

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,13 +6,15 @@ import java.util.List;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     //private final QuestionRepository questionRepository;
     private final QuestionService questionService;
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
@@ -22,14 +24,14 @@ public class QuestionController {
 
     @GetMapping("/")
     public String root(){
-        return "redirect:/question/list";
+        return "redirect:/list";
     }
     /*
     redirect:<url> URL로 리다이렉트 (리다이렉트는 완전히 새로운 URL로 요청이 된다.)
     forward:<url> URL로 포워드 (포워드는 기존 요청 값들이 유지된 상태로 URL이 전환된다.)
      */
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
